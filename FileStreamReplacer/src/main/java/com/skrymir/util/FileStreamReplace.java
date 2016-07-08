b5413f8d7cb45f8ede012155fd109b0cb125612f
Joshua Colbeck <skrymir@gmail.com>
2016-07-08 10:22:30 -0500

    Added file move

--- FileStreamReplacer/src/main/java/com/skrymir/util/FileStreamReplace.java
+++ FileStreamReplacer/src/main/java/com/skrymir/util/FileStreamReplace.java
@@ -2,8 +2,11 @@ package com.skrymir.util;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
 
 public class FileStreamReplace {
@@ -19,12 +22,18 @@ public class FileStreamReplace {
 	}
 
 	public static void removeDoubleQuotes(String fileName) throws IOException {
+		Path inputFilePath = Paths.get(fileName);
+		Path outputFilePath = Paths.get(fileName + "_fixed");
 		// try-with-resources -- let java close my stream and writer!
 		// files can be big, read line by line
-		try (Stream<String> inputStream = Files.lines(Paths.get(fileName));
-				PrintWriter outputWriter = new PrintWriter(fileName + "_fixed", "UTF-8")) {
+		try (Stream<String> inputStream = Files.lines(inputFilePath);
+				PrintWriter outputWriter = new PrintWriter(outputFilePath.toString(), "UTF-8")) {
 			inputStream.map(s -> s.replaceAll("\"", "")) // do the replace
 			.forEachOrdered(outputWriter::println);		 // write fixed line to new file
 		}
+		
+		Files.move(outputFilePath, outputFilePath.resolveSibling(inputFilePath.getFileName()), StandardCopyOption.REPLACE_EXISTING);
 	}
+	
+	
 }
