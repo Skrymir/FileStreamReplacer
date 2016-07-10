0412b798f1e1eb0e27f000c49b64815469f68c55
Josh Colbeck <skrymir@gmail.com>
2016-07-10 11:51:27 -0500

    test merging

--- src/main/java/com/skrymir/util/FileStreamReplace.java
+++ src/main/java/com/skrymir/util/FileStreamReplace.java
@@ -35,7 +35,7 @@ public class FileStreamReplace {
 	 */
 	public static void removeDoubleQuotes(String fileName) throws IOException {
 		Path inputFilePath = Paths.get(fileName);
-		Path outputFilePath = Paths.get(fileName + "_fixed");
+		Path outputFilePath = Paths.get(fileName + "_fixxxed");
 		// try-with-resources -- let java close my stream and writer!
 		// files can be big, read line by line
 		try (Stream<String> inputStream = Files.lines(inputFilePath);
